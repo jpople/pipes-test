eb7a255cc2cd70c26959d1c3062e357f9f465452
Jeremy Pople <jpople@gmail.com>
2021-04-23 10:29:15 -0400

    refactored main map class; also add support for tile connection arrays

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
+// important nomenclature note: make sure you're properly differentiating "tiles" and "sprites"
+
 var canvas = document.getElementById("main");
 var context = canvas.getContext("2d");
 
+var tilemap = { // these should be hardcoded based on the image file and never change during running
+  image: "tilemap.png",
+  cols: 4,
+  rows: 4,
+  spriteSize: 16,
+  spriteConnections: [
+    // values start from the top side of the sprite in its default rotation and go clockwise
+    [null, "blue", null, "blue"],
+    [null, null, "blue", "blue"],
+    [null, "blue", "blue", "blue"],
+    ["blue", "blue", "blue", "blue"],
+    [null, "orange", null, "orange"],
+    [null, null, "orange", "orange"],
+    [null, "orange", "orange", "orange"],
+    ["orange", "orange", "orange", "orange",],
+    ["orange", "blue", "orange", "blue"],
+    ["blue", "orange", "orange", "blue"],
+    [null, null, null, "blue"],
+    [null, null, null, "orange"],
+    [null, null, null, null],
+  ],
+  rotateConnections: function(index, rotation) {
+    var duped = this.spriteConnections[index].concat(this.spriteConnections[index]);
+    var result = duped.slice(rotation, rotation + 4);
+    return result;
+  }
+}
+
 var image = new Image(128, 128);
-image.src = "tilemap.png"
+image.src = tilemap.image;
 
 image.onload = function() {
-  for (var r = 0; r < map.outputRows; r++) {
-    for (var c = 0; c < map.outputCols; c++) {
+  for (var r = 0; r < map.rows; r++) {
+    for (var c = 0; c < map.cols; c++) {
       var tile = map.getTile(c, r);
-      console.log(tile);
 
-      var tileX = (tile.spriteId % map.spriteCols) * map.spriteSize;
-      var tileY = (Math.floor(tile.spriteId / map.spriteCols)) * map.spriteSize;
-
+      var tileX = (tile.spriteId % tilemap.cols) * tilemap.spriteSize;
+      var tileY = (Math.floor(tile.spriteId / tilemap.cols)) * tilemap.spriteSize;
+      // save correct connection array to gamestate
+      map.tileConnections[(r * map.cols) + c] = tilemap.rotateConnections(tile.spriteId, tile.rotation);
+      console.log(map.tileConnections[(r * map.cols) + c]);
       // clearRect where tile will go (not necessary yet)
       // save default transform
       context.save();
       // move transform to center of cell at row, col
-      context.translate((c + 0.5) * map.outputSize, (r + 0.5) * map.outputSize);
+      context.translate((c + 0.5) * map.tileSize, (r + 0.5) * map.tileSize);
       // rotate transform appropriate amount
       context.rotate(tile.rotation * Math.PI / 2);
       // draw image around center point
@@ -25,43 +56,41 @@ image.onload = function() {
         image, // image
         tileX, // source x
         tileY, // source y
-        map.spriteSize, // source width
-        map.spriteSize, // source height
-        -0.5 * map.outputSize, // target x
-        -0.5 * map.outputSize, // target y
-        map.outputSize, // target width
-        map.outputSize // target height
+        tilemap.spriteSize, // source width
+        tilemap.spriteSize, // source height
+        -0.5 * map.tileSize, // target x
+        -0.5 * map.tileSize, // target y
+        map.tileSize, // target width
+        map.tileSize // target height
       );
-      //restore previous transform
+      //reset transform
       context.restore();
     }
   }
 }
 
-canvas.onload = function() {
-}
-
 var map = {
-    spriteCols: 4, // # of columns on spritesheet
-    spriteRows: 4, // # of rows on spritesheet
-    outputCols: 3, // # of columns on canvas
-    outputRows: 3, // # of rows on canvas
-    spriteSize: 16, // size of tile on spritesheet
-    outputSize: 128, // size of tile on output canvas
-    tileIdentities: [ // index on spritesheet of tile to be drawn
-      12, 12, 12, 
-      10, 0, 10, 
-      12, 12, 12, 
+    cols: 4, // # of columns on canvas
+    rows: 4, // # of rows on canvas
+    tileSize: 128, // size of tile on output canvas
+    tileIndices: [ // index on spritesheet of tile to be drawn
+      12, 12, 12, 12,
+      10, 0, 0, 10,
+      11, 4, 4, 11,
+      12, 12, 12, 12, 
     ],
-    tileRotations: [ // # of times tile should be rotated 90° clockwise before drawing (from 0-3)
-      0, 0, 0, 
-      2, 0, 0, 
-      0, 0, 0,
+    tileRotations: [ // # of times sprite should be rotated 90° clockwise before drawing (from 0-3)
+      0, 0, 0, 0,
+      2, 0, 0, 0,
+      2, 0, 0, 0,
+      0, 0, 0, 0
     ],
+    tileConnections: [],
     getTile: function(col, row) { // gets information required to draw tile by position
-        var index = (row * map.outputCols) + col;
+        // probably nice to have access to the index elsewhere... is there a particular reason this needs to take col, row instead of just index?
+        var index = (row * map.cols) + col;
         return {
-            spriteId: this.tileIdentities[index],
+            spriteId: this.tileIndices[index],
             rotation: this.tileRotations[index]
         }
     }
