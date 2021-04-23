93cc1c8db50e69bb5fcba6762a0b2aec077a9b61
Jeremy Pople <jpople@gmail.com>
2021-04-22 22:25:52 -0400

    add support for rotating tiles

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ var image = new Image(128, 128);
 image.src = "tilemap.png"
 
 image.onload = function() {
-  for (var r = 0; r < map.rows; r++) {
-    for (var c = 0; c < map.cols; c++) {
-      var tile = map.getTile(c, r).spriteId;
+  for (var r = 0; r < map.outputRows; r++) {
+    for (var c = 0; c < map.outputCols; c++) {
+      var tile = map.getTile(c, r);
+      console.log(tile);
 
-      var tileX = (tile % map.cols) * map.spriteSize;
-      var tileY = (Math.floor(tile / map.cols)) * map.spriteSize;
+      var tileX = (tile.spriteId % map.spriteCols) * map.spriteSize;
+      var tileY = (Math.floor(tile.spriteId / map.spriteCols)) * map.spriteSize;
+
+      // clearRect where tile will go (not necessary yet)
+      // save default transform
+      context.save();
+      // move transform to center of cell at row, col
+      context.translate((c + 0.5) * map.outputSize, (r + 0.5) * map.outputSize);
+      // rotate transform appropriate amount
+      context.rotate(tile.rotation * Math.PI / 2);
+      // draw image around center point
       context.drawImage(
         image, // image
         tileX, // source x
         tileY, // source y
         map.spriteSize, // source width
         map.spriteSize, // source height
-        c * map.outputSize, // target x
-        r * map.outputSize, // target y
+        -0.5 * map.outputSize, // target x
+        -0.5 * map.outputSize, // target y
         map.outputSize, // target width
         map.outputSize // target height
       );
+      //restore previous transform
+      context.restore();
     }
   }
 }
@@ -30,24 +42,24 @@ canvas.onload = function() {
 }
 
 var map = {
-    cols: 4, // # of columns on canvas
-    rows: 4, // # of rows on canvas
+    spriteCols: 4, // # of columns on spritesheet
+    spriteRows: 4, // # of rows on spritesheet
+    outputCols: 3, // # of columns on canvas
+    outputRows: 3, // # of rows on canvas
     spriteSize: 16, // size of tile on spritesheet
     outputSize: 128, // size of tile on output canvas
     tileIdentities: [ // index on spritesheet of tile to be drawn
-      0, 1, 2, 3,
-      4, 5, 6, 7,
-      8, 9, 10, 11,
-      0, 0, 0, 0
+      12, 12, 12, 
+      10, 0, 10, 
+      12, 12, 12, 
     ],
-    tileRotations: [ // # of times tile should be rotated 90° clockwise before drawing (0-3)
-      0, 0, 0, 0,
-      0, 0, 0, 0, 
-      0, 0, 0, 0, 
-      0, 0, 0, 0
+    tileRotations: [ // # of times tile should be rotated 90° clockwise before drawing (from 0-3)
+      0, 0, 0, 
+      2, 0, 0, 
+      0, 0, 0,
     ],
     getTile: function(col, row) { // gets information required to draw tile by position
-        var index = (row * map.cols) + col;
+        var index = (row * map.outputCols) + col;
         return {
             spriteId: this.tileIdentities[index],
             rotation: this.tileRotations[index]
